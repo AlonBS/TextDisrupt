ab330ab078874707c7ff183ba740579d693eabf3
Lior Resisi <liorr@liveperson.com>
2017-05-05 02:08:43 +0300

    Change style

--- app/src/main/java/com/reuth/hack/textdisrupt/MainActivity.java
+++ app/src/main/java/com/reuth/hack/textdisrupt/MainActivity.java
@@ -63,16 +63,6 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(getApplicationContext(), FingerPaint.class);
-                getApplicationContext().startActivity(myIntent);
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
