6af8faf3c19027a69a6b18613f9df1c0d9392b3e
Lior Resisi <liorr@liveperson.com>
2017-05-05 04:33:13 +0300

    UI changes

--- app/src/main/java/com/reuth/hack/textdisrupt/BottomBarFragment.java
+++ app/src/main/java/com/reuth/hack/textdisrupt/BottomBarFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
